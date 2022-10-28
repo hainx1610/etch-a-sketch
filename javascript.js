c5b474f380fbb4d76b19692bdaa010ca370b87c1
hainx1610 <hainx1610@gmail.com>
2022-10-28 18:00:56 +0700

    create function to change square color when hover mouse

--- javascript.js
+++ javascript.js
@@ -2,17 +2,21 @@ const grid = document.querySelector(".grid");
 const gridHeight = Math.round(parseFloat(getComputedStyle(grid).height));
 const gridWidth = Math.round(parseFloat(getComputedStyle(grid).width));
 
+const addHover = element => {
+    element.addEventListener("mouseover", e => {
+        element.classList.add("mousedover");
+    })
+}
+
 function createSquare(n) {
     for (let i = 1; i <= n ** 2; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         square.style.height = gridHeight/n  + "px";
         square.style.width = gridWidth/n  + "px";
+        addHover(square);
         grid.appendChild(square);
     }
 }
 
 createSquare(16);
-
-
-
